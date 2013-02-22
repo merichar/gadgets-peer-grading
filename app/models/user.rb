ea413787dc9cfe4b809859877c4361bf4a1a4392
Meg Richards <mouse@cmu.edu>
2013-02-22 05:12:51 -0500

    Add LDAP integration to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,34 @@ class User < ActiveRecord::Base
 
   attr_accessible :andrewid, :username
 
+
+  def ldap_reference
+    @ldap_reference ||= CarnegieMellonPerson.find_by_andrewid( self.andrewid )
+    # Add new attributes to CarnegieMellonPerson attributes before adding 
+    # references to them in participant.rb
+  end
+
+  def name
+    Array.[](ldap_reference["cn"]).flatten.last
+  end
+
+  def surname
+    ldap_reference["sn"]
+  end
+  
+  def email
+    ldap_reference["mail"]
+  end
+
+  def department
+    ldap_reference["cmuDepartment"]
+  end
+
+  def student_class
+    ldap_reference["cmuStudentClass"]
+  end
+
+
   validates_presence_of :andrewid
   validates_presence_of :username
 
---
+++ test/unit/carnegie_mellon_person_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CarnegieMellonPersonTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
